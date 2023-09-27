e55b1e121b445c3b9325f4f1456cf8aa26ecb775
José Manuel Molero <jmlero@hotmail.com>
2023-09-27 21:36:08 +0200

    Update app.py

--- simplebackend-python/app.py
+++ simplebackend-python/app.py
@@ -1,11 +1,50 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/hola', methods=['GET'])
-def hola():
-    data = {"key": "value"}
-    return jsonify(data)
+# This is a simulated in-memory database for our shop items.
+shop_items = [
+    {"id": 1, "name": "apple", "price": 0.5, "stock": 100},
+    {"id": 2, "name": "banana", "price": 0.25, "stock": 50},
+]
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+@app.route('/shop/items', methods=['GET'])
+def get_items():
+    return jsonify(shop_items), 200
+
+@app.route('/shop/items', methods=['POST'])
+def add_item():
+    data = request.json
+    if not data or 'name' not in data or 'price' not in data or 'stock' not in data:
+        return jsonify({"message": "Invalid data."}), 400
+
+    new_item = {
+        "id": len(shop_items) + 1,
+        "name": data['name'],
+        "price": float(data['price']),
+        "stock": int(data['stock'])
+    }
+    shop_items.append(new_item)
+    return jsonify(new_item), 201
+
+@app.route('/shop/items/reduce', methods=['POST'])
+def reduce_stock():
+    data = request.json
+    if not data or 'id' not in data or 'amount' not in data:
+        return jsonify({"message": "Invalid data."}), 400
+
+    item_id = data['id']
+    amount = data['amount']
+
+    item = next((item for item in shop_items if item['id'] == item_id), None)
+    if not item:
+        return jsonify({"message": "Item not found."}), 404
+
+    if item['stock'] - amount < 0:
+        return jsonify({"message": "Stock can't be negative."}), 400
+
+    item['stock'] -= amount
+    return jsonify(item), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
