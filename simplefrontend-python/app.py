73cf79945beb3bc5b374dfdfcbbf5a5ccb356ab6
José Manuel Molero <jmlero@hotmail.com>
2023-09-30 19:38:39 +0200

    Create app.py

---
+++ simplefrontend-python/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+# Backend URL
+BACKEND_URL = "http://flask-shop-app-service:80/shop/items"
+
+
+@app.route('/')
+def index():
+    try:
+        response = requests.get(BACKEND_URL)
+        items = response.json()
+    except:
+        items = []
+
+    return render_template('index.html', items=items)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
